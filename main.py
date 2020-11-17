d1eed9570cb827e1f81f2580539d412d09734db3
fodela <particleme30@gmail.com>
2020-11-17 22:49:13 +0000

    added the api connection and the weather update functionality

--- main.py
+++ main.py
@@ -2,38 +2,58 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 from kivy.lang import Builder
-# from kivy.uix.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
+from datetime import date
+import requests, json
 
 # Builder.load_file('weather.kv')
 
 class HomeScreen(Screen):
-    # city_name = StringProperty()
-    # county_name = StringProperty()
-    # temp_c = StringProperty()
-    # weather_text = StringProperty()
-    # is_day = StringProperty()
-    # def search(self):
-    #     try:
-    #         apikey = f"https://api.weatherapi.com/v1/current.json?key=a5b634c8339b46b5a60161317203110&q={city.get()}"
+    city_name = StringProperty()
+    country_name = StringProperty()
+    temp_c = StringProperty()
+    weather_text = StringProperty()
+    is_day = StringProperty()
+    img_url = StringProperty()
+    humidity = StringProperty()
+    wind = StringProperty()
+    pressure = StringProperty()
+    feels_like = StringProperty()
+    sunset = StringProperty()
+    Today = StringProperty()
+    today = date.today()
+    
+    
+    def search(self,instance,keycode):
+        self.Today = self.today.strftime("%a, %d %b")
+        city = self.ids.searched_city.text
+        try:
+            apikey = f"https://api.weatherapi.com/v1/current.json?key=a5b634c8339b46b5a60161317203110&q={city}"
 
-    #         api_request = requests.get(apikey)
-    #         api_response = json.loads(api_request.content)
+            api_request = requests.get(apikey)
+            api_response = json.loads(api_request.content)
 
-    #         city_name = f"{api_response['location']['name']}"
-    #         country_name = f"{api_response['location']['country']}"
-    #         temp_c =f"{api_response['current']['temp_c']}" 
-    #         img_url = f"http:{api_response['current']['condition']['icon']}"
-    #         weather_text = f"{api_response['current']['condition']['text']}"
-    #         is_day = f"{api_response['current']['is_day']}"
-    #     except Exception as e:
-    #         print(e)
-    pass
+            self.city_name = f"{api_response['location']['name']}"
+            self.country_name = f"{api_response['location']['country']}"
+            temp =f"{api_response['current']['temp_c']}" 
+            self.img_url = f"http:{api_response['current']['condition']['icon']}"
+            self.weather_text = f"{api_response['current']['condition']['text']}"
+            self.is_day = f"{api_response['current']['is_day']}"
+            self.temp_c = temp[:2]
+            self.humidity = f"{api_response['current']['humidity']}"
+            self.wind = f"{api_response['current']['wind_mph']}"
+            self.pressure = f"{api_response['current']['pressure_in']}"
+            self.feels_like = f"{api_response['current']['feelslike_c']}"
+            self.sunset = f"{api_response['astronomy']['astro']['sunset']}"
+        except Exception as e:
+            print(e)
 
 
 class WeatherApp(MDApp):
     def __init__(self):
         super().__init__()
         Window.size = (350,600)
+        
 
 if __name__ == "__main__":
     WeatherApp().run()
