1620d1318ec52d5815fae7368d5a0f713960f955
fodela <particleme30@gmail.com>
2020-12-04 07:31:03 +0000

    created the weather screen which displays the weather details

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import StringProperty, ObjectProperty
 from datetime import date
 import requests, json
 
-# Builder.load_file('weather.kv')
 
-class HomeScreen(Screen):
+class Index(Screen):
+    pass
+
+class Weather(Screen):
+    pass
+    
+
+
+
+class WeatherApp(MDApp):
+    def __init__(self,*args, **kwargs):
+        super().__init__()
+        Window.size = (350,600)
+        self.location = ''
+
+    def build(self):
+        self.sm = ScreenManager()
+
+        self.index_page = Index()
+        screen = Screen(name='index')
+        screen.add_widget(self.index_page)
+        self.sm.add_widget(screen)
+
+        self.weather_page = Weather()
+        screen = Screen(name='weather')
+        screen.add_widget(self.weather_page)
+        self.sm.add_widget(screen)
+
+        return self.sm
+        
+    def change_screen(self,screen_name):
+        self.sm.current = screen_name
+        
+        
     city_name = StringProperty()
     country_name = StringProperty()
     temp_c = StringProperty()
@@ -19,16 +51,17 @@ class HomeScreen(Screen):
     wind = StringProperty()
     pressure = StringProperty()
     feels_like = StringProperty()
-    sunset = StringProperty()
+    uv = StringProperty()
     Today = StringProperty()
     today = date.today()
-    
-    
     def search(self,instance,keycode):
+        if self.sm.current == 'index':
+            location = self.index_page.ids.searched_city.text
+        elif self.sm.current == 'weather':
+            location = self.weather_page.ids.searched_city.text
         self.Today = self.today.strftime("%a, %d %b")
-        city = self.ids.searched_city.text
         try:
-            apikey = f"https://api.weatherapi.com/v1/current.json?key=a5b634c8339b46b5a60161317203110&q={city}"
+            apikey = f"https://api.weatherapi.com/v1/current.json?key=a5b634c8339b46b5a60161317203110&q={location}"
 
             api_request = requests.get(apikey)
             api_response = json.loads(api_request.content)
@@ -44,16 +77,10 @@ class HomeScreen(Screen):
             self.wind = f"{api_response['current']['wind_mph']}"
             self.pressure = f"{api_response['current']['pressure_in']}"
             self.feels_like = f"{api_response['current']['feelslike_c']}"
-            self.sunset = f"{api_response['astronomy']['astro']['sunset']}"
+            self.uv = f"{api_response['current']['uv']}"
+            self.change_screen('weather')
         except Exception as e:
             print(e)
-
-
-class WeatherApp(MDApp):
-    def __init__(self):
-        super().__init__()
-        Window.size = (350,600)
-        
 
 if __name__ == "__main__":
     WeatherApp().run()
