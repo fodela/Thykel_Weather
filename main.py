0c268f089c3fc2d098295495378b561fc4676f16
fodela <particleme30@gmail.com>
2021-01-02 01:43:36 +0000

    i think i am done for now

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from datetime import date
 import requests, json
 
 
-class Index(Screen):
-    pass
+# class Index(Screen):
+#     pass
 
 class Weather(Screen):
     pass
@@ -20,7 +20,7 @@ class LocationGetter(Screen):
 
 
 
-class WeatherApp(MDApp):
+class Thykel_WeatherApp(MDApp):
     city_name = StringProperty()
     country_name = StringProperty()
     temp_c = StringProperty()
@@ -34,6 +34,9 @@ class WeatherApp(MDApp):
     uv = StringProperty()
     Today = StringProperty()
     today = date.today()
+    current_city = StringProperty('Ho')
+    current_country = StringProperty('Ghana')
+    background_pic = StringProperty('images/bgpic.jpg' )
 
     def __init__(self,*args, **kwargs):
         super().__init__()
@@ -41,24 +44,12 @@ class WeatherApp(MDApp):
         self.location = ''
         self.icon = '/home/fodela/Project/Python Projects/Kivy Projects/Thykel_Weather/images/weather_icon.png'
         self.ask_current_location_dialog = None
-
-    def build(self):
-        self.sm = ScreenManager()
-
-        self.index_page = Index()
-        screen = Screen(name='index')
-        screen.add_widget(self.index_page)
-        self.sm.add_widget(screen)
-
-        self.weather_page = Weather()
-        screen = Screen(name='weather')
-        screen.add_widget(self.weather_page)
-        self.sm.add_widget(screen)
-
-        return self.sm
-    
+        self.err_dialog = None
+  
     def on_start(self):
-        self.ask_current_location()
+        location = f"{self.current_city}, {self.current_country} "
+        self.get_data(location)
+        self.background_pic = 'images/bgpic.jpg'
 
     def ask_current_location(self):
         
@@ -72,7 +63,7 @@ class WeatherApp(MDApp):
                                             buttons=[
                                                 MDFlatButton(
                                                     text = 'Cancel',
-                                                    # on_press = lambda x: self.cancel()
+                                                    on_press = lambda x: self.cancel()
                                                 ),
                                                 MDRaisedButton(
                                                     text = 'Add',
@@ -82,14 +73,11 @@ class WeatherApp(MDApp):
                                             )
         self.ask_current_location_dialog.open()
         
-    def change_screen(self,screen_name):
-        self.sm.current = screen_name
-        
     def search(self,instance,keycode):
-        if self.sm.current == 'index':
-            location = self.index_page.ids.searched_city.text
-        elif self.sm.current == 'weather':
-            location = self.weather_page.ids.searched_city.text
+        location = self.root.ids.searched_city.text
+        self.get_data(location)
+        
+    def get_data(self,location):
         self.Today = self.today.strftime("%a, %d %b")
         try:
             apikey = f"https://api.weatherapi.com/v1/current.json?key=a5b634c8339b46b5a60161317203110&q={location}"
@@ -99,24 +87,42 @@ class WeatherApp(MDApp):
 
             self.city_name = f"{api_response['location']['name']}"
             self.country_name = f"{api_response['location']['country']}"
-            temp =f"{api_response['current']['temp_c']}" 
+            self.temp_c =f"{int(api_response['current']['temp_c'])}" 
             self.img_url = f"http:{api_response['current']['condition']['icon']}"
             self.weather_text = f"{api_response['current']['condition']['text']}"
             self.is_day = f"{api_response['current']['is_day']}"
-            self.temp_c = temp[:2]
             self.humidity = f"{api_response['current']['humidity']}"
             self.wind = f"{api_response['current']['wind_mph']}"
             self.pressure = f"{api_response['current']['pressure_in']}"
             self.feels_like = f"{api_response['current']['feelslike_c']}"
             self.uv = f"{api_response['current']['uv']}"
-            self.change_screen('weather')
+            
+        except Exception as e:
+            self.error_dialog(str(e))
+            print(e)
+    
+    def close_dialog(self):
+        try:
+            self.ask_current_location_dialog.dismiss(force=True)
         except Exception as e:
             print(e)
 
     def add_location(self):
-        c = LocationGetter().ids.current_city.text
-        # C = 
-        print(c)
+        location = f"{self.current_city}, {self.current_country} "
+        self.get_data(location)
+        self.close_dialog()
+
+    def cancel(self):
+        self.close_dialog()
+
+    def error_dialog(self,err):
+        self.err_dialog = MDDialog(
+                                            title = 'Error!',
+                                            type = 'custom',
+                                            text = err,
+                                            size_hint = (.8,.3)
+                                            )
+        self.err_dialog.open()
 
 if __name__ == "__main__":
-    WeatherApp().run()
+    Thykel_WeatherApp().run()
